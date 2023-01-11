dbf76dc55fa42f8659938928e82b42d47be17d0b
Arnab Barua <117771679+dawninfo@users.noreply.github.com>
2023-01-12 00:20:42 +0530

    Create projects.js

---
+++ assets/js/projects.js
@@ -0,0 +1,37 @@
+const projects = [
+    {
+        "name": "Pets",
+        "description": "A mini-simulator built into dawn. Coded and maintained by Sparky Child.",
+        "url": "https://dawnfeatures.glitch.me/pets.html"
+    },
+    {
+        "name": "Bucks",
+        "description": "The economy of Dawn!",
+        "url": "https://dawnfeatures.glitch.me/bucks.html"
+    },
+    {
+        "name": "EXP & Levels",
+        "description": "Work in Progress.",
+        "url": ""
+    },
+    {
+        "name": "Gangs",
+        "description": "Work in Progress.",
+        "url": ""
+    },
+    {
+        "name": "Azuremons",
+        "description": "Work in Progress.",
+        "url": ""
+    },
+    {
+        "name": "Leagues",
+        "description": "Work in Progress.",
+        "url": ""
+    },
+    {
+        "name": "PSGO Card",
+        "description": "Work in Progress.",
+        "url": ""
+    },
+]
