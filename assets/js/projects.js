4f4cff00bdb07484910ac81624e60d47f1a15ba7
Arnab Barua <117771679+dawninfo@users.noreply.github.com>
2023-01-21 15:40:26 +0530

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,31 +2,31 @@ const projects = [
     {
         "name": "Pets",
         "description": "A mini-simulator built into dawn. Coded and maintained by us.",
-        "url": "https://dawnfeatures.glitch.me/pets.html"
+        "url": "https://dawninfo.github.io/pets.html"
     },
     {
         "name": "Bucks",
         "description": "The economy of Dawn! You can earn them by winning tournaments and events.",
-        "url": "https://dawnfeatures.glitch.me/bucks.html"
+        "url": "https://dawninfo.github.io/bucks.html"
     },
     {
         "name": "EXP & Levels",
         "description": "Earn experience, reach new levels and unlock more features!",
-        "url": "https://dawnfeatures.glitch.me/exp.html"
+        "url": "https://dawninfo.github.io/exp.html"
     },
     {
         "name": "Gangs",
         "description": "Join your favorite gang and fight for victory!",
-        "url": "https://dawnfeatures.glitch.me/gangs.html"
+        "url": "https://dawninfo.github.io/gangs.html"
     },
     {
         "name": "Azuremons",
         "description": "Create your favorite pokemon with almost endless customization of moves and abilities!",
-        "url": "https://dawnfeatures.glitch.me/azuremons.html"
+        "url": "https://dawninfo.github.io/azuremons.html"
     },
     {
         "name": "PSGO Cards",
         "description": "Work in Progress.",
-        "url": "https://dawnfeatures.glitch.me/psgo.html"
+        "url": "https://dawninfo.github.io/psgo.html"
     },
 ]
